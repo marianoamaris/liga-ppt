dcfd554f85a386d39acc29396a39c5e6f9d9f621
marianoamaris <mariano32josea@gmail.com>
2025-09-04 11:43:43 -0500

    Revert "Fix: Remove missing index.css import to fix build"
    This reverts commit 362b2103b3a7abffd4d39856239c0a67d3e2a178.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 import { AppRouter } from "./Router";
 
 createRoot(document.getElementById("root")!).render(
