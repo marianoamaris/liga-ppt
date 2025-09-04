362b2103b3a7abffd4d39856239c0a67d3e2a178
marianoamaris <mariano32josea@gmail.com>
2025-09-04 11:41:25 -0500

    Fix: Remove missing index.css import to fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { AppRouter } from "./Router";
 
 createRoot(document.getElementById("root")!).render(
