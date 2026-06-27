db41d76e7febb7e07ca081c63918afe082489e00
Mariano Amaris <sirama@Marianos-MacBook-Air.local>
2026-06-26 22:19:47 -0500

    feat: jornada 6

--- src/constants/PROXIMA_JORNADA_LIGA19.ts
+++ src/constants/PROXIMA_JORNADA_LIGA19.ts
@@ -11,11 +11,11 @@ export interface FixtureJornada {
 }
 
 export const PROXIMA_JORNADA_LIGA19: FixtureJornada = {
-  jornada: 5,
-  fecha: "Jueves 25 de Junio",
+  jornada: 6,
+  fecha: "Jueves 2 de Julio",
   grupos: [
-    { lugar: "La Jugada", cancha: 4, equiposIds: ["francia", "noruega", "mexico"] },
-    { lugar: "O. Geles",  cancha: 2, equiposIds: ["argentina", "corea-del-sur", "brasil"] },
-    { lugar: "O. Geles",  cancha: 3, equiposIds: ["portugal", "paises-bajos", "alemania"] },
+    { lugar: "La Jugada", cancha: 4, equiposIds: ["noruega", "argentina", "alemania"] },
+    { lugar: "O. Geles",  cancha: 2, equiposIds: ["mexico", "paises-bajos", "francia"] },
+    { lugar: "O. Geles",  cancha: 3, equiposIds: ["brasil", "corea-del-sur", "portugal"] },
   ],
 };
